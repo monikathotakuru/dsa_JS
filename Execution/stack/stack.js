e17cb382e8375c9cbd2a99c54b123d23ecf4a8c6
mounika <monika.thotakuru@gmail.com>
2020-05-30 09:48:36 +0530

    stack implementation using LL

--- Execution/stack/stack.js
+++ Execution/stack/stack.js
@@ -1,8 +1,9 @@
 // implementation with array
 const stack1 = require('../../stack/arrayImplementation.js');
-
+const stack2 = require('../../stack/LLimplementation.js')
 let stackEx1  = new stack1(6);
 
+stackEx1.push(6);
 stackEx1.push(10);
 console.log('pushed into stack')
 stackEx1.push(20);
@@ -39,4 +40,41 @@ stackEx1.deleteStack();
 
 
 
-// implementation with linked lists
+// stack implementation with linked lists
+stackEx1  = new stack2(6);
+stackEx1.push(10);
+console.log('pushed into stack')
+stackEx1.push(20);
+console.log('pushed into stack')
+stackEx1.push(30);
+console.log('pushed into stack')
+stackEx1.push(40);
+console.log('pushed into stack')
+stackEx1.push(50);
+stackEx1.push(60);
+stackEx1.push(30);
+console.log(stackEx1.isEmpty());
+
+console.log(stackEx1.peek());
+
+
+console.log(stackEx1.pop());
+console.log(stackEx1.pop());
+console.log(stackEx1.peek());
+
+console.log(stackEx1.pop());
+console.log(stackEx1.pop());
+console.log(stackEx1.isEmpty());
+
+console.log(stackEx1.peek());
+
+console.log(stackEx1.pop());
+console.log(stackEx1.pop());
+console.log(stackEx1.pop());
+console.log(stackEx1.pop());
+stackEx1.deleteStack();
+stackEx1.push(30);
+
+console.log(stackEx1);
+
+
--- stack/LLimplementation.js
+++ stack/LLimplementation.js
@@ -1,1 +1,51 @@
 // stack implementation using linked lists
+class stackUsingLL {
+    constructor(value) {
+        let node = {value: value, next: null};
+        this.stack = {
+            head: node, 
+            tail: node
+        }
+    }
+
+    push(value) {
+	let node = {value: value, next: null};
+        if (this.stack.head === null) { 
+	    this.stack.head = node;
+            this.stack.tail = node;
+	}
+        this.stack.tail.next = node;
+	this.stack.tail = node;
+    }
+    pop() {
+	if (this.stack.head === null) return 'no element in stack';
+        let node = this.stack.tail;
+	let tmpnode = this.stack.head;
+	if (tmpnode.next === null) {
+	    this.stack.head = null;
+	    this.stack.tail = null;
+	} else {
+	    while (tmpnode.next !== node) {
+		tmpnode = tmpnode.next;
+	    }
+	    tmpnode.next = null
+	    this.stack.tail = tmpnode;
+	}
+	
+	return node.value;
+    }
+    peek() {
+        if (this.stack.tail === null) return 'no elements in stack';
+        else return this.stack.tail.value;
+    }
+    isEmpty() {
+        return this.stack.head === null;
+    }
+    deleteStack() {
+        this.stack.head = null;
+        this.stack.tail = null;
+    }
+
+}
+
+module.exports = stackUsingLL;
